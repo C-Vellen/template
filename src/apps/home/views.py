20118b93c63fdcc23b19894ec070ff8d4fc529de
Christophe Vellen <christophe.vellen@gmail.com>
2026-05-12 17:03:15 +0200

    django-tailwind fix

--- src/apps/home/views.py
+++ src/apps/home/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from .context import usercontext
 
 
-@login_required
 def index(request):
     context = usercontext(request)
     context.update(
--- src/theme/static_src/tailwind.config.js
+++ src/theme/static_src/tailwind.config.js
@@ -1,45 +1,15 @@
-/**
- * This is a minimal config.
- *
- * If you need the full config, get it from here:
- * https://unpkg.com/browse/tailwindcss@latest/stubs/defaultConfig.stub.js
- */
+const colors = require('tailwindcss/colors')
 
 module.exports = {
     content: [
-        /**
-         * HTML. Paths to Django template files that will contain Tailwind CSS classes.
-         */
-
-        /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
         '../templates/**/*.html',
-
-        /*
-         * Main templates directory of the project (BASE_DIR/templates).
-         * Adjust the following line to match your project structure.
-         */
+        
         '../../templates/**/*.html',
-
-        /*
-         * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
-         * Adjust the following line to match your project structure.
-         */
         '../../**/templates/**/*.html',
-
-        /**
-         * JS: If you use Tailwind CSS in JavaScript, uncomment the following lines and make sure
-         * patterns match your project structure.
-         */
-        /* JS 1: Ignore any JavaScript in node_modules folder. */
-        // '!../../**/node_modules',
-        /* JS 2: Process all JavaScript files in the project. */
-        // '../../**/*.js',
-
-        /**
-         * Python: If you use Tailwind CSS classes in Python, uncomment the following line
-         * and make sure the pattern below matches your project structure.
-         */
-        // '../../**/*.py'
+        '../../../**/templates/**/*.html',
+        '../src/**/*.css',
+        '../src/**/*.js',
+        '../src/**/*.py',
     ],
     theme: {
         extend: {
@@ -49,8 +19,9 @@ module.exports = {
             action2: actions secondaires
             access:  boutons pour les droits d'accès */
             colors: {
-                margin: '#333',
-                action: '#333',
+                page: colors.slate[100],
+                action: colors.slate[600],
+                actionHover: colors.slate[500],
                 action2: '#86a',
                 access: '#0ff',
             },
